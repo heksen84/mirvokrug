6e1c506c25d5561abce12076f5d28c7ab23b6760
Илья <heksen84@yandex.ru>
2018-04-21 16:31:26 +0600

    ok

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,16 +1,19 @@
 require('./bootstrap');
 
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import welcome from './views/welcome.vue'
 import home from './views/home.vue'
+import profile from './views/profile.vue'
 import newtrip from './views/newtrip.vue'
 
+Vue.use(Vuetify);
 Vue.use(BootstrapVue);
 
 const app = new Vue({
     el: '#app',
-    components: { welcome, home, newtrip }
+    components: { welcome, profile, home, newtrip }
 });
