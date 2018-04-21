c8e6364af4037823615ed8c7eada3f78c5eb85d9
Илья <heksen84@yandex.ru>
2018-04-21 19:40:38 +0600

    ok

--- resources/assets/js/views/Welcome.vue
+++ resources/assets/js/views/Welcome.vue
@@ -3,23 +3,7 @@
     <v-app>
      <v-content>
        <v-container>
-
-  <v-layout>
-   <v-flex xs12 sm6 offset-sm3>
-     <v-card>
-       <v-card-title primary-title>
-         <div>
-           <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-           <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-         </div>
-       </v-card-title>
-       <v-card-actions>
-         <v-btn flat color="orange">Share</v-btn>
-         <v-btn flat color="orange">Explore</v-btn>
-       </v-card-actions>
-     </v-card>
-   </v-flex>
- </v-layout>
+         <h1>Мир вокруг</h1>
 
        </v-container>
      </v-content>
