816e9aeff2a6b1216c25a68c1ac678201993d236
Илья <heksen84@yandex.ru>
2018-04-22 10:42:32 +0600

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
   <meta name="description" content="get free numbers">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>МИР ВОКРУГ - онлайн-сервис для путешественников</title>
+  <title>Мир на ладонях - онлайн-сервис для путешественников</title>
   <link rel="icon" href="{{ asset('public/globe.ico') }}">
 <!--<link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">-->
 </head>
