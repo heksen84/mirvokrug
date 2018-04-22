fd39ea88aaed16c4feb89ac5fe9c7b2ea72f3c8f
Илья <heksen84@yandex.ru>
2018-04-22 20:05:00 +0600

    ok

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,7 +9,7 @@ import welcome from './views/welcome.vue'
 import home from './views/home.vue'
 import profile from './views/profile.vue'
 import newtrip from './views/newtrip.vue'
-import passwordreset from './views/passwordreset.vue'
+import passwordreset from './views/auth/passwordreset.vue'
 
 import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
